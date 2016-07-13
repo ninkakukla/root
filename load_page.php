57f8c92fbcc9094032dacb5a7e9cf03cfe00d2d1
nina.tobolkin <tobolkin8@hotmail.com>
2016-07-13 11:25:14 +0300

    fix session on page load

--- load_page.php
+++ load_page.php
@@ -5,9 +5,10 @@ if(!$_POST['page']) die("0");
 $page = (int)$_POST['page'];
 
 if(file_exists('view/sketch/page_'.$page.'.php')){
-	session_unset($_SESSION['empty']);
-	session_unset($_SESSION['error']);
-	session_unset($_SESSION['wrongName']);
+	if(isset($_SESSION['empty']))session_unset($_SESSION['empty']);
+	if(isset($_SESSION['error']))session_unset($_SESSION['error']);
+	if(isset($_SESSION['wrongName']))session_unset($_SESSION['wrongName']);
+
 	echo file_get_contents('view/sketch/page_'.$page.'.php');
 }
 
